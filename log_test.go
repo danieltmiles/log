2dfddd3c34d5db1f46f5426248536d43ec2628d0
Daniel Hess <dan9186@gmail.com>
2015-03-24 14:03:31 -0700

    stubbing tests with gomega goblin

--- log_test.go
+++ log_test.go
@@ -1,1 +1,13 @@
 package log
+
+import (
+	"testing"
+
+	. "github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+)
+
+func TestLogging(t *testing.T) {
+	g := Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+}
