67f603fcd31d5ff5fcd7baeeb0dfc707d1b0b7af
Daniel Hess <dan9186@gmail.com>
2015-03-24 14:08:20 -0700

    adding threshold and turning into object

--- log.go
+++ log.go
@@ -2,55 +2,93 @@ package log
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"sync"
 	"time"
 )
 
-var (
-	tag string
-	mu  sync.Mutex
+type Logger struct {
+	mu        sync.Mutex
+	tag       string
+	threshold Level
+	writer    io.Writer
+}
+
+type Level int8
+
+const (
+	None Level = iota - 1
+	Fatal
+	Error
+	Warning
+	Notice
+	Info
+	Debug
 )
 
-func init() {
-	tag = os.Args[0]
+var (
+	levelStrings = []string{
+		"FATAL",
+		"ERROR",
+		"WARNING",
+		"NOTICE",
+		"INFO",
+		"DEBUG",
+	}
+)
+
+func New(writer io.Writer, threshold Level) *Logger {
+	return &Logger{
+		writer:    writer,
+		threshold: threshold,
+	}
 }
 
-func SetTag(t string) {
-	tag = t
+func (log *Logger) SetTag(t string) {
+	log.tag = t
 }
 
-func Debug(msg string) {
-	write("DEBUG", msg)
+func (log *Logger) Debug(msg string) {
+	log.write(Debug, msg)
 }
 
-func Error(msg string) {
-	write("ERROR", msg)
+func (log *Logger) Info(msg string) {
+	log.write(Info, msg)
 }
 
-func Fatal(msg string) {
-	write("FATAL", msg)
+func (log *Logger) Notice(msg string) {
+	log.write(Notice, msg)
+}
+
+func (log *Logger) Warning(msg string) {
+	log.write(Warning, msg)
+}
+
+func (log *Logger) Error(msg string) {
+	log.write(Error, msg)
+}
+
+func (log *Logger) Fatal(msg string) {
+	log.write(Fatal, msg)
 	os.Exit(1)
 }
 
-func Info(msg string) {
-	write("INFO", msg)
-}
+func (log *Logger) write(level Level, msg string) {
+	if level < log.threshold {
+		return
+	}
 
-func Notice(msg string) {
-	write("NOTICE", msg)
-}
-
-func Warning(msg string) {
-	write("WARNING", msg)
-}
-
-func write(level, msg string) {
 	timestamp := time.Now().Format(time.RFC3339)
 	hostname, _ := os.Hostname()
-	mu.Lock()
-	defer mu.Unlock()
 
-	fmt.Fprintf(os.Stderr, "%s %s %s[%d]: %s %s\n",
-		timestamp, hostname, tag, os.Getpid(), level, msg)
+	log.mu.Lock()
+	defer log.mu.Unlock()
+
+	fmt.Fprintf(log.writer, "%s %s %s[%d]: %s %s\n",
+		timestamp, hostname, log.tag, os.Getpid(), level, msg)
+}
+
+func (level Level) String() string {
+	return levelStrings[level]
 }
