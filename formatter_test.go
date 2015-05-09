4a315cd546c6fc9bba7367a9f011651db745673a
Daniel Hess <dan9186@gmail.com>
2015-05-08 17:15:39 -0700

    improving coverage

--- formatter_test.go
+++ formatter_test.go
@@ -29,5 +29,24 @@ func TestFormatter(t *testing.T) {
 			line := f.Format(Debug, m)
 			Expect(line).To(ContainSubstring(fmt.Sprintf("%s %s[%d]: %s [%v]", h, t, p, Debug, m)))
 		})
+
+		g.It("should allow setting the tag", func() {
+			h, _ := os.Hostname()
+			t := os.Args[0]
+			p := os.Getpid()
+			m := "test debug message"
+
+			f := &DefaultFormat{
+				hostname: h,
+				pid:      p,
+				tag:      t,
+			}
+
+			newT := "CoolApp v1.0.0"
+			f.SetTag(newT)
+
+			line := f.Format(Debug, m)
+			Expect(line).To(ContainSubstring(fmt.Sprintf("%s %s[%d]: %s [%v]", h, newT, p, Debug, m)))
+		})
 	})
 }
